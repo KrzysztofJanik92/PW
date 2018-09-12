049965eef5cea30b96bb9980265e695ed5c03021
Krzysztof Janik <krzysztof.janik@vp.pl>
2018-09-12 20:00:33 +0200

    pierwszy commit

---
+++ JS/menu.js
@@ -0,0 +1,19 @@
+var mn = $("#mainNav");
+var mobile = $('#hamburger');
+mns = "main-nav-scrolled";
+hdr = $('.header').height();
+
+
+$(window).scroll(function () {
+    if ($(this).scrollTop() > hdr) {
+        mn.addClass(mns);
+    } else {
+        mn.removeClass(mns);
+    }
+});
+
+mobile.click(function () {
+    var nav = $('#mobileNav');
+
+    nav.slideToggle(200);
+});
